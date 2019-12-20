944e73b12fd8f16953b08da74e43c2ad3ae9ad90
Nicholas <n3ttl3t@gmail.com>
2019-12-20 12:11:17 +0000

    recognises noses but requires them

--- lib/CountSmileys.rb
+++ lib/CountSmileys.rb
@@ -2,10 +2,13 @@ def CountSmileys(args)
     # acceptableChars = [':', ';', ')', 'D', '-', '~']
     result = 0
     eyes = [':', ';']
+    noses = ['-', '~']
     for smiley in args
         features = smiley.split('')
         if eyes.include? features[0]
-            result += 1
+            if noses.include? features[1]
+                result += 1
+            end
         end
     end
     result
