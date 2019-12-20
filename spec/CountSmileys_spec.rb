714f1cc938abef3c9165543df954a42b545a941a
Nicholas <n3ttl3t@gmail.com>
2019-12-20 11:55:25 +0000

    second test failing

--- spec/CountSmileys_spec.rb
+++ spec/CountSmileys_spec.rb
@@ -3,4 +3,8 @@ describe 'CountSmileys' do
     it 'Returns 1 when one valid smiley is input' do
         expect(CountSmileys([':)'])).to eq 1
     end
+
+    it 'Returns 2 when two valid smileys are input' do
+        expect(CountSmileys([':)', ';-D'])).to eq 2
+    end
 end
