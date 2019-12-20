317ee29da80e468ac1c35b5b21df17ab3da5e89f
Nicholas <n3ttl3t@gmail.com>
2019-12-20 12:21:51 +0000

    recognises eyes, optional noses, and mouths

--- lib/CountSmileys.rb
+++ lib/CountSmileys.rb
@@ -8,7 +8,13 @@ def CountSmileys(args)
         features = smiley.split('')
         if eyes.include? features[0]
             if noses.include? features[1] or mouths.include? features[1]
-                result += 1
+                if features.length == 3
+                    if mouths.include? features[2]
+                        result += 1
+                    end
+                else
+                    result += 1
+                end
             end
         end
     end
